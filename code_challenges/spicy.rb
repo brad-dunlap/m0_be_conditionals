caca883d41aea38de1e0abdc85177ed4500fd08a
Brad Dunlap <braddunlapdpt@gmail.com>
2022-11-02 16:41:30 -0400

    test 1

--- code_challenges/spicy.rb
+++ code_challenges/spicy.rb
@@ -5,8 +5,7 @@
 #of 5, print "Buzz". If the Integer is a multiple of both 3 and 5, print "FizzBuzz". If the Integer 
 #is not a multiple of either, print the Integer itself.
 
-
-integer = 4
+integer = 3
 
 if (integer%3).zero? == true && (integer%5).zero? == true
     puts "FizzBuzz"
