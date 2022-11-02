9d0f48f0031502e74dc9a5b6eb56038a85d439a0
Brad Dunlap <braddunlapdpt@gmail.com>
2022-11-02 16:46:10 -0400

    Add new code for spicy

--- code_challenges/spicy.rb
+++ code_challenges/spicy.rb
@@ -5,14 +5,14 @@
 #of 5, print "Buzz". If the Integer is a multiple of both 3 and 5, print "FizzBuzz". If the Integer 
 #is not a multiple of either, print the Integer itself.
 
-integer = 3
 
-if (integer%3).zero? == true && (integer%5).zero? == true
+integer = 5
+if (integer%3.zero?) == true && (integer%5).zero? == true
     puts "FizzBuzz"
-elsif (integer%3).zero? == true
+elsif (integer%3).zero == true
     puts "Fizz"
-elsif (integer%5).zero? == true
+elsif (integer%5).zero == true
     puts "Buzz"
-else 
+else
     puts "#{integer}"
 end
