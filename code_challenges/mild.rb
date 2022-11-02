edf5ce4e58149f5a12240dbccbfae55382e8f193
Brad Dunlap <braddunlapdpt@gmail.com>
2022-11-02 14:22:30 -0400

    Test

--- code_challenges/mild.rb
+++ code_challenges/mild.rb
@@ -3,7 +3,7 @@
 #Write a Ruby program that defines a variable that stores an Integer. The program should print out the 
 #String "even" if the Integer is even, and the String "odd" if the Integer is odd.
 
-integer = 2
+integer = 7
 
 if integer.odd? == true
 puts "odd"
