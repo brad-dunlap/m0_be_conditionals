6a4e0f379e502adb153150415a973231b33d49f8
Brad Dunlap <braddunlapdpt@gmail.com>
2022-11-02 14:39:26 -0400

    Test code 2

--- code_challenges/spicy.rb
+++ code_challenges/spicy.rb
@@ -8,11 +8,11 @@
 
 integer = 5
 
-if (integer%3) == true && (integer%5) == true
+if (integer%3).zero? == true && (integer%5).zero? == true
     puts "FizzBuzz"
-elsif (integer%3) == true
+elsif (integer%3).zero? == true
     puts "Fizz"
-elsif (integer%5) == true
+elsif (integer%5).zero? == true
     puts "Buzz"
 else puts #{integer}
 end
